1dac8beaa5483458a54e95e924d023da9c776dec
Muhammad Aatif <skybridgecx@gmail.com>
2026-08-11 20:52:37 -0400

    I18: bind controller release to reconciled branch

--- tools/project_agent_runtime/controller_release.py
+++ tools/project_agent_runtime/controller_release.py
@@ -18,7 +18,7 @@ from .controller import (
 )
 
 
-CONTROLLER_RELEASE_REF = "tooling/youmo-cli-i17"
+CONTROLLER_RELEASE_REF = "tooling/youmo-cli-i18"
 EXTRA_LAUNCHERS: tuple[str, ...] = (
     "youmo-doctor",
     "youmo-resume",
